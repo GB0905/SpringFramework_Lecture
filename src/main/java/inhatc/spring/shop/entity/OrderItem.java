d3abe19e262d58e82b6b3254971f361a2ef43062
GGB0905 <102276041+GGB0905@users.noreply.github.com>
2023-11-12 18:42:17 +0900

    11주차 실습

--- src/main/java/inhatc/spring/shop/entity/OrderItem.java
+++ src/main/java/inhatc/spring/shop/entity/OrderItem.java
@@ -3,6 +3,8 @@ package inhatc.spring.shop.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Setter
@@ -25,4 +27,12 @@ public class OrderItem {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
+
+    private int orderPrice; // 주문 가격
+
+    private int count;      // 수량
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
 }
